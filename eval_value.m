0387c1b19dbd29144f3872d1866e61da147afae0
Teodora Plopeanu <152170132+teodoraplopeanu@users.noreply.github.com>
2024-10-28 19:34:57 +0200

    Update eval_value.m

--- eval_value.m
+++ eval_value.m
@@ -1,8 +1,4 @@
 function pred = eval_value(x, X, f, f_param, a)
-   # TO DO: pentru un vector de input x, preziceti valoarea acestuia in
-   # in functie de toti vectorii de input folositi pentru a antrena modelul
-   # folosind functia de kernel f care are ca al 3-lea parametru f_param
-   # si vectorul coloana a
    n = size(X, 1);
    pred = 0;
    
