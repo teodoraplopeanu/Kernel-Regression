c50192d8102f9505e78c722038189e9a86957008
Teodora Plopeanu <152170132+teodoraplopeanu@users.noreply.github.com>
2024-10-28 19:36:46 +0200

    Update split_dataset.m

--- split_dataset.m
+++ split_dataset.m
@@ -1,5 +1,5 @@
 function [X_train, y_train, X_pred, y_pred] = split_dataset (X, y, percentage)
-  # TO DO: împarte setul de date în 2 seturi: 
+  # Imparte setul de date în 2 seturi: 
   # un set de training si un set de test,
   # ambele reprezentate printr-o matrice de features un vector de clase
   # percentage este un parametru considerat intre 0 si 1
