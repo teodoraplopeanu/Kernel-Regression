69d0f6d56cae11e333061d66baa2cda896cfd6ce
Teodora Plopeanu <152170132+teodoraplopeanu@users.noreply.github.com>
2024-10-28 19:34:17 +0200

    Update build_kernel.m

--- build_kernel.m
+++ build_kernel.m
@@ -1,8 +1,4 @@
 function [K] = build_kernel (X, f, f_param)
-  # Construiti matricea K (matricea kernel-urilor) asa cum este
-  # descrisa in enuntul temei pornind de la datele de intrare X
-  # Functia de kernel este descrisa de parametrul f si foloseste f_param
-  # ca al 3-lea parametru
   n = size(X, 1);
   K = zeros(n, n);
   
