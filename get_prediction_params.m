7c11f6e46a8baf510e73cdca335ce3e7da329f5e
Teodora Plopeanu <152170132+teodoraplopeanu@users.noreply.github.com>
2024-10-28 19:35:40 +0200

    Update get_prediction_params.m

--- get_prediction_params.m
+++ get_prediction_params.m
@@ -1,6 +1,4 @@
 function [a] = get_prediction_params (K, y, lambda)
-  # TO DO: folosind metode neiterative, implementati logica
-  # pentru a obtine vectorul coloana a, asa cum este descris in enuntul temei
   [m, m] = size(K);
   A = lambda * eye(m) + K;
 
